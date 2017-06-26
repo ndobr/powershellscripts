7d73f43bd46bed0db4463b0756d9f52bce9a4dd7
COGNET\natalia.dobrzanska <natalia.dobrzanska@cognifide.com>
2017-06-26 13:46:10 +0200

    Remove inline style script added

---
+++ remove inline style.ps1
@@ -0,0 +1,7 @@
+
+
+$regex = '<img[^>]+(style=".*?")'
+$items = Get-ChildItem -Path "/sitecore/content/Rapha Nocturne/Global/home" -Recurse | Where-Object {$_.Content -match $regex } | ForEach-Object {
+    Write-Host $_.Paths.Path
+    $_.Content = $_.Content.Replace([regex]::Match($_.Content, $regex).Groups.Item(1), '')
+}

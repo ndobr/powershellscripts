027a2172d6b1d9609ba998b0d7fce9c54eab04b1
COGNET\natalia.dobrzanska <natalia.dobrzanska@cognifide.com>
2017-06-29 15:59:12 +0200

    small fixes in style script

--- Add style without modal.ps1
+++ Add style without modal.ps1
@@ -2,5 +2,5 @@ $name = Show-Input "Provide style name"
 $NewStyle= New-Item -Path "/sitecore/content/Showcase/int/Presentation/Styles" -ItemType "/sitecore/templates/Foundation/Experience Accelerator/Presentation/Style" -Name $name
 
 $NewStyle.Editing.BeginEdit()
-$NewStyle.Fields["__Valuse"].Value = test2
+$NewStyle.Fields["Value"].Value = "test2"
 $NewStyle.Editing.EndEdit()

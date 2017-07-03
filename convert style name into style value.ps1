fdd888a4c677f274b86d7d38f259c4fe6d20b610
COGNET\natalia.dobrzanska <natalia.dobrzanska@cognifide.com>
2017-07-03 10:04:17 +0200

    Script updated

--- convert style name into style value.ps1
+++ convert style name into style value.ps1
@@ -1,3 +1,7 @@
 $StyleName = Show-Input "Please provide new style name:"
 $LowerStyleName = $StyleName.ToLower()
 Write-Host $LowerStyleName
+$NewItem = New-Item -Path "/sitecore/content/Showcase/int/Presentation/Styles" -ItemType "/sitecore/templates/Foundation/Experience Accelerator/Presentation/Style" -Name $StyleName
+$NewItem.Editing.BeginEdit()
+$NewItem.Fields["Value"].Value = $LowerStyleName
+$NewItem.Editing.EndEdit()

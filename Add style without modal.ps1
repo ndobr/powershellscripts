a7cf6b19bfd8665e8e32ce2b3df6c5e9b23cc847
COGNET\natalia.dobrzanska <natalia.dobrzanska@cognifide.com>
2017-07-04 13:00:37 +0200

    Deleted script

--- Add style without modal.ps1
+++
@@ -1,6 +0,0 @@
-$name = Show-Input "Provide style name"
-$NewStyle= New-Item -Path "/sitecore/content/Showcase/int/Presentation/Styles" -ItemType "/sitecore/templates/Foundation/Experience Accelerator/Presentation/Style" -Name $name
-
-$NewStyle.Editing.BeginEdit()
-$NewStyle.Fields["Value"].Value = "test2"
-$NewStyle.Editing.EndEdit()

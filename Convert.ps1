5362cf5edc0c00c3135bbd5b9f71b64f3487bd1a
COGNET\natalia.dobrzanska <natalia.dobrzanska@cognifide.com>
2017-08-07 11:41:45 +0200

    Convert script updated

--- Convert.ps1
+++ Convert.ps1
@@ -1,7 +1,9 @@
-$StyleName = Show-Input "Please provide new style name:"
+﻿$StyleName = Show-Input "Please provide new style name:"
 $LowerStyleName = $StyleName.ToLower()
 Write-Host $LowerStyleName
-$NewItem = New-Item -Path "/sitecore/content/Showcase/int/Presentation/Styles" -ItemType "/sitecore/templates/Foundation/Experience Accelerator/Presentation/Style" -Name $StyleName
+$NewItem = Read-Variable -Parameters @{ Name = "l"; Path ="p"; Title = "Pick Item"; Source="/sitecore/content/Showcase/int/Presentation/Styles"; editor="droptree"; }
+Write-Host $l.Paths.Path
+$NewItem = New-Item $l.Paths.Path -ItemType "/sitecore/templates/Foundation/Experience Accelerator/Presentation/Style" -Name $StyleName
 $NewItem.Editing.BeginEdit()
 $NewItem.Fields["Value"].Value = $LowerStyleName
 $NewItem.Fields["Value"].Value = $NewItem.Fields["Value"].Value.Replace(“ “,”-”)

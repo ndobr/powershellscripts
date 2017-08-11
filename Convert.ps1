6d0a1085857561fd47b3b85f8752f9fe811ab5ff
COGNET\natalia.dobrzanska <natalia.dobrzanska@cognifide.com>
2017-08-11 08:54:38 +0200

    script fixed

--- Convert.ps1
+++ Convert.ps1
@@ -1,9 +1,11 @@
 ﻿$StyleName = Show-Input "Please provide new style name:"
-$LowerStyleName = $StyleName.ToLower()
+if ($StyleName -eq $null) { Exit }
 $Source = (Get-Item .).Paths.Path
+$LowerStyleName = $StyleName.ToLower().Replace(“ “,”-”)
 $NewItem = New-Item $Source -ItemType "/sitecore/templates/Foundation/Experience Accelerator/Presentation/Style" -Name $StyleName
-$NewItem = New-Item $l.Paths.Path -ItemType "/sitecore/templates/Foundation/Experience Accelerator/Presentation/Style" -Name $StyleName
 $NewItem.Editing.BeginEdit()
 $NewItem.Fields["Value"].Value = $LowerStyleName
-$NewItem.Fields["Value"].Value = $NewItem.Fields["Value"].Value.Replace(“ “,”-”)
+Read-Variable -Parameters @{ Name="l"; Title="Pick Rendering"; Source="/sitecore/layout/Renderings/Feature/Experience Accelerator"; Editor="TreelistEx"; }  -Height 620 -Width 600
+$NewItem.Fields["Allowed Renderings"].Value = $l.ID
+$NewItem.Fields["Allowed Renderings"].Value = $NewItem.Fields["Allowed Renderings"].Value.Replace(" ","|")
 $NewItem.Editing.EndEdit()
